ce3424ea3fae258f37923507971347738ee49f8f
AdamShay <62936242+AdamShay@users.noreply.github.com>
2021-05-03 00:58:35 +0100

    Add files via upload

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,9 +5,23 @@ using UnityEngine;
 public class Enemy : Interactable
 {
     // Start is called before the first frame update
+    PlayerManager playerManager;
+    CharacterStats myStats;
+    
+
+    void start ()
+    {
+        playerManager = PlayerManager.instance;
+        myStats = getComponent<CharacterStats>();
+
+    }
     public override void Interact()
     {
         base.Interact();
-        // Attacking enemy 
+        CharacterCombat playerCombat = playerManager.player.GetComponent<CharacterComabt>();
+        if(playerCombat != null)
+        {
+            playerCombat.Attack(myStats);
+        }
     }
 }
